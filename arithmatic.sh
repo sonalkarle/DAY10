cff68895d4f1a3b1982623d7c4d3d16018c71d82
sonal <sonalkarle01@gmail.com>
2020-12-11 01:58:11 +0530

    Store the result in a dictionary

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,3 +1,25 @@
 #!/bin/bash -x
 echo "Arithmetic Computation & sorting "
+read -p "Enter First Number : " a
+read -p "Enter second Number : " b
+read -p "Enter third Number:" c
+
+sum=$(($a+($b*$c)))
+echo $sum
+
+sum1=$((($a*$b)+$c))
+echo $sum1
+
+sum2=$(($c+($a/$b)))
+echo $sum2
+
+sum3=$((($a%$b)+$c))
+echo $sum3
+ 
+declare -A dict
+
+dict[1]=`echo $sum`
+dict[2]=`echo $sum1`
+dict[3]=`echo $sum2`
+dict[4]=`echo $sum3`
 
