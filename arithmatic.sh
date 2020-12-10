a53cfc6351a5603fd79600dc61a3f9fbc1da30df
sonal <sonalkarle01@gmail.com>
2020-12-11 01:37:02 +0530

    compute (a%b+c)

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 echo "Arithmetic Computation & sorting "
+read -p "Enter First Number : " a
+read -p "Enter second Number : " b
+read -p "Enter third Number:" c
 
+sum=$(($a+($b*$c)))
+echo $sum
+
+sum1=$((($a*$b)+$c))
+echo $sum1
+
+sum2=$(($c+($a/$b)))
+echo $sum2
+
+sum3=$((($a%$b)+$c))
+echo $sum3
+ 
