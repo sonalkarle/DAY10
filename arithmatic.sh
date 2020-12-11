17e59e628742a1ea0429acab9d2b30b7573b8dc3
sonal <sonalkarle01@gmail.com>
2020-12-11 23:27:30 +0530

    Added code to disply computed values array in desending order

--- arithmatic.sh
+++ arithmatic.sh
@@ -23,3 +23,25 @@ dict[2]=`echo $sum1`
 dict[3]=`echo $sum2`
 dict[4]=`echo $sum3`
 
+for value in "${dict[@]}"
+do
+	valsInArr[$arrayIndex]=$value
+	arrayIndex=$(( $arrayIndex + 1 ))
+done
+arrLength=${#valsInArr[@]}
+
+
+for (( i = 0 ; i <= $(( $arrLength - 1 )) ; i++ ))
+do
+	for (( j = 0 ; j < $i ;j++ ))
+	do
+		if [ `echo | awk -v iVal="${valsInArr[$i]}" -v jVal="${valsInArr[$j]}" '{ if( iVal >= jVal ) print "True" ; else print "False" ;}'` == True ]
+		then
+		Temp=${valsInArr[$i]}
+		valsInArr[$i]=${valsInArr[$j]}
+		valsInArr[$j]=$Temp
+		fi
+	done
+done
+
+echo ${valsInArr[@]}
