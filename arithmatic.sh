eda2b226ee97c967ac90970383e4eb9fa1cc8a99
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-25 02:48:11 +0530

    Update arithmatic.sh

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
+## Computation of a,b and c
 echo "Arithmetic Computation & sorting "
-
+##vriable
 read -p "Enter First Number : " a
 read -p "Enter second Number : " b
 read -p "Enter third Number:" c
-
+#computation
 sum=$(($a+($b*$c)))
 echo $sum
 
