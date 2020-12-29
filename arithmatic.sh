d92da7ced73ffd112a502db6ffccff7928478ac3
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-29 23:06:28 +0530

    Update arithmatic.sh

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
+##Read the values from dictionary into array and sort in decending 
+##Read the input from user
 echo "Arithmetic Computation & sorting "
 read -p "Enter First Number : " a
 read -p "Enter second Number : " b
 read -p "Enter third Number:" c
-<<<<<<< HEAD
-=======
 
+##computation
 sum=$(($a+($b*$c)))
 echo $sum
 
@@ -17,27 +18,7 @@ echo $sum2
 
 sum3=$((($a%$b)+$c))
 echo $sum3
- 
-declare -A dict
-
-dict[1]=`echo $sum`
-dict[2]=`echo $sum1`
-dict[3]=`echo $sum2`
-dict[4]=`echo $sum3`
->>>>>>> master
-
-sum=$(($a+($b*$c)))
-echo $sum
-
-sum1=$((($a*$b)+$c))
-echo $sum1
-
-sum2=$(($c+($a/$b)))
-echo $sum2
-
-sum3=$((($a%$b)+$c))
-echo $sum3
- 
+ ##declare dictionary 
 declare -A dict
 
 dict[1]=`echo $sum`
